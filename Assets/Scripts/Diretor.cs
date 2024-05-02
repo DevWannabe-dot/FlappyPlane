c249231496af0bca16d6024c8bbfaf377dba0a4a
Pedro H <spyxtreme41@gmail.com>
2024-05-02 15:32:24 -0300

    Adicionar botão para reinicializar

--- Assets/Scripts/Diretor.cs
+++ Assets/Scripts/Diretor.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Diretor : MonoBehaviour
 {
+    [SerializeField] private GameObject imagemGameOver;
     public void finalizarJogo(){
         Time.timeScale = 0;
+        // habilitar a imagem de Game Over
+        this.imagemGameOver.SetActive(true);
     }
 }
