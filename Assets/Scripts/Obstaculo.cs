a99e60ee346e4894c1841c186cd0d28987a356d4
Pedro H <spyxtreme41@gmail.com>
2024-05-02 14:43:28 -0300

    Adicionar RigidBody2D, desserializar a velocidade.

--- Assets/Scripts/Obstaculo.cs
+++ Assets/Scripts/Obstaculo.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Obstaculo : MonoBehaviour
 {
-    [SerializeField] private float velocidade = 5f;
+    private float velocidade = 5f;
     private float variacaoDaPosicaoY = 1f;
     
     private void Awake()
