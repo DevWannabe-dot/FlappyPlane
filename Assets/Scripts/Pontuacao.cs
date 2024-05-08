83ad976a803a99a9faf4cd4fcf86d3490da6fb1d
Pedro H <spyxtreme41@gmail.com>
2024-05-08 13:52:19 -0300

    Adicionar trilha sonora

--- Assets/Scripts/Pontuacao.cs
+++ Assets/Scripts/Pontuacao.cs
@@ -7,7 +7,12 @@ public class Pontuacao : MonoBehaviour
 {
     [SerializeField] private Text Pontuacao_Texto;
     private int pontos;
+    private AudioSource audioPontuacao;
 
+    private void Awake()
+    {
+        this.audioPontuacao = this.GetComponent<AudioSource>();
+    }
     public void AdicionarPontos()
     {
         this.pontos++;
@@ -17,5 +22,6 @@ public class Pontuacao : MonoBehaviour
     {
         this.pontos = 0;
         this.Pontuacao_Texto.text = this.pontos.ToString();
+        this.audioPontuacao.Play();
     }
 }
